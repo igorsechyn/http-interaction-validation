9a7db29c61de1616fea7f1c4d8f046ea7b84c924
Igor Sechyn <igor@linear.app>
2022-11-21 12:14:09 +0200

    chore: removed comment

--- config.go
+++ config.go
@@ -4,7 +4,6 @@ type config struct {
 	requestValidationConfig *requestValidationConfig
 }
 
-// dummy commit another
 type requestValidationConfig struct {
 	preservePayload      bool
 	payloadValue         interface{}
